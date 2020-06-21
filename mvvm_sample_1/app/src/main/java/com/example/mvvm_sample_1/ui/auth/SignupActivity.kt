63f8ffb952f9ee8a554bd4a28e129eebd3e3d346
EANimesha <nimeshadilini999@gmail.com>
2020-06-21 19:31:08 +0530

    create the folder structure and added UI for activities

--- mvvm_sample_1/app/src/main/java/com/example/mvvm_sample_1/ui/auth/SignupActivity.kt
+++ mvvm_sample_1/app/src/main/java/com/example/mvvm_sample_1/ui/auth/SignupActivity.kt
@@ -1,12 +1,13 @@
-package com.example.mvvm_sample_1
+package com.example.mvvm_sample_1.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvm_sample_1.R
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
